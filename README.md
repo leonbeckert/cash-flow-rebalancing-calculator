36dd0e4f728c57409f385ceb09b075dab031e130
Leon Beckert <leon.beckert@posteo.de>
2023-01-26 15:40:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,174 @@
+<!-- PROJECT SHIELDS -->
+<!--
+*** We're using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <h3 align="center">Cash Flow Rebalancing Calculator</h3>
+
+  <p align="center">
+    by Leon Beckert
+    <br />
+    <br />
+    <a href="https://github.com/leonbeckert/cash-flow-rebalancing-calculator/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/leonbeckert/cash-flow-rebalancing-calculator/issues">Request Feature</a>
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+-   [About the Project](#about-the-project)
+    -   [Built With](#built-with)
+-   [Getting Started](#getting-started)
+    -   [Prerequisites](#prerequisites)
+    -   [Installation](#installation)
+-   [Roadmap](#roadmap)
+-   [Contributing](#contributing)
+-   [License](#license)
+-   [Contact](#contact)
+-   [Acknowledgements](#acknowledgements)
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+"Cash Flow Rebalancing" promises the advantages (additional returns) of regular portfolio rebalancing without its disadvantages (taxes, transaction costs).
+
+In classic portfolio rebalancing, a rebalancing adjustment occurs once every quarter, semester or yearly. Assets that have performed better and are now overweight get sold off and the resulting cash inflow gets invested in the underperformers. The result is a completely rebalanced portfolio.
+
+The disadvantages of this approach are clear: Gains have to be realized, taxes have to be paid and transaction costs occur for every asset.
+
+"Cash Flow Rebalancing" helps us defer taxation and reduce transaction cost.
+
+By only investing in the assets that negatively deviate the most from our target distribution we do not need to sell any assets. This can be done on a monthly basis in our savings phase and reversely (selling the overweight assets) in the consumption phase.
+
+In the savings phase we are deffering taxation indefinitely in a buy-and-hold strategy because we do not realize any gains. At the time of writing (2023), monthly savings plans on ETFs are widely available and mostly free in the western world. Using this strategy, we can expect 0.1% to 0.4% additional yearly returns when rebalancing the same asset class: globally diversified stock ETFs (Gerd Kommer 2018).
+
+This cash flow rebalancing calculator offers users the ability to set up their portfolio structure initially within the python executable only returning monthly (or yearly) to calculate their cash flow rebalancing amounts.
+
+[![Cash Flow Rebalancing Calculator Screen Shot 1][product-screenshot-1]][product-screenshot-url-1]
+
+Rebalancing fully with an amount that is larger than the necessary rebalancing amount
+
+[![Cash Flow Rebalancing Calculator Screen Shot 2][product-screenshot-2]][product-screenshot-url-2]
+
+Rebalancing fully in detail with the exact amount needed
+
+[![Cash Flow Rebalancing Calculator Screen Shot 3][product-screenshot-3]][product-screenshot-url-3]
+
+Approximating our target distributions with less cash flow
+
+### Built With
+
+-   [Python](https://www.python.org)
+-   [NumPy](https://numpy.org)
+-   [prettytable](https://pypi.org/project/prettytable/)
+
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+This is a list of things you need to use the software and how to install them.
+
+-   [Python](https://www.python.org/downloads/)
+
+### Installation
+
+1. Clone the repo
+
+```sh
+git clone https://github.com/leonbeckert/cash-flow-rebalancing-calculator.git
+```
+
+2. Install NumPy and prettytable
+
+```sh
+pip install numpy
+```
+```sh
+pip install prettytable
+```
+
+3. Run python script
+
+```sh
+python cash_flow_rebalancing_calculator.py
+```
+
+<!-- ROADMAP -->
+
+## Roadmap
+
+See the [open issues](https://github.com/leonbeckert/cash-flow-rebalancing-calculator/issues) for a list of proposed features (and known issues).
+
+<!-- CONTRIBUTING -->
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+Before contributing please read our [Code Of Conduct](https://github.com/leonbeckert/cash-flow-rebalancing-calculator/blob/main/CODE_OF_CONDUCT.md).
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<!-- LICENSE -->
+
+## License
+
+This project is licensed under the [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/). See `LICENSE` for more information.
+
+<!-- CONTACT -->
+
+## Contact
+
+email: [beckert@leon.fm](mailto:beckert@leon.fm)
+
+Project Link: [https://github.com/leonbeckert/cash-flow-rebalancing-calculator](https://github.com/leonbeckert/cash-flow-rebalancing-calculator)
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## Acknowledgements
+
+Creator:
+
+-   [Leon Beckert (Linkedin)](https://www.linkedin.com/in/leon-beckert-15508a18b/)
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+[contributors-shield]: https://img.shields.io/github/contributors/leonbeckert/cash-flow-rebalancing-calculator.svg?style=flat-square
+[contributors-url]: https://github.com/leonbeckert/cash-flow-rebalancing-calculator/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/leonbeckert/cash-flow-rebalancing-calculator.svg?style=flat-square
+[forks-url]: https://github.com/leonbeckert/cash-flow-rebalancing-calculator/network/members
+[stars-shield]: https://img.shields.io/github/stars/leonbeckert/cash-flow-rebalancing-calculator.svg?style=flat-square
+[stars-url]: https://github.com/leonbeckert/cash-flow-rebalancing-calculator/stargazers
+[issues-shield]: https://img.shields.io/github/issues/leonbeckert/cash-flow-rebalancing-calculator.svg?style=flat-square
+[issues-url]: https://github.com/leonbeckert/cash-flow-rebalancing-calculator/issues
+[license-shield]: https://img.shields.io/github/license/leonbeckert/cash-flow-rebalancing-calculator.svg?style=flat-square
+[license-url]: https://creativecommons.org/licenses/by/4.0/deed.de
+[product-screenshot-url-1]: images/screenshot.png
+[product-screenshot-url-2]: images/screenshot2.png
+[product-screenshot-url-3]: images/screenshot3.png
---
+++ cash_flow_rebalancing_calculator.py
@@ -0,0 +1,211 @@
+"""Cash flow rebalancing app"""
+import numpy as np
+from prettytable import PrettyTable
+
+
+def correct_input(investments, distributions):
+    """Checks whether the input is formatted correctly"""
+    # Check whether number of investments and distributions match
+    if not len(investments) == distributions.size:
+        return False
+    # Check whether total distribution adds up to 100%
+    if distributions.sum() != 1:
+        return False
+    # If all checks run successfully exit with success
+    return True
+
+
+def calculate_current_distributions(investments):
+    """Calculates the current distributions"""
+    current_distributions = []
+    total_amount = 0
+    for investment in investments:
+        total_amount += investment
+    for investment in investments:
+        current_distributions.append(round(investment / total_amount, 8))
+    return current_distributions
+
+
+def find_highest_positive_deviation(investments, distributions):
+    """Finds the stock deviating positively the most from its target percentage"""
+    highest_positive_deviation = 0
+    highest_positive_deviation_location = 0
+    current_distributions = calculate_current_distributions(investments)
+    for counter, current_distribution in enumerate(current_distributions):
+        temp_deviation = current_distribution - distributions[counter]
+        if highest_positive_deviation < temp_deviation:
+            highest_positive_deviation = temp_deviation
+            highest_positive_deviation_location = counter
+    return highest_positive_deviation_location
+
+
+def calculate_minimum_rebalancing_amounts(investments, distrib_targets):
+    """Calculates the minimum amount necessary to rebalance to the highest value"""
+    max_dev_loc = find_highest_positive_deviation(investments, distrib_targets)
+    total_target = investments[max_dev_loc] / distrib_targets[max_dev_loc]
+    single_necessary_investments = []
+    for counter, distribution in enumerate(distrib_targets):
+        single_necessary_investments.append(
+            total_target * distribution - investments[counter])
+    return single_necessary_investments
+
+
+def calculate_exact_rebalancing_amounts(investment_capital, post_comma_round,
+    amounts, distribution_targets):
+    """Calculates the exact rebalancing amounts for each position"""
+    rebalancing_investments = calculate_minimum_rebalancing_amounts(
+        amounts, distribution_targets)
+    if not sufficient_rebalancing_capital(investment_capital, rebalancing_investments):
+        return False, []
+    remaining_capital = investment_capital
+    for amount in rebalancing_investments:
+        remaining_capital -= amount
+    post_rebalancing_amount_per_position = remaining_capital / \
+        len(rebalancing_investments)
+    for counter, amount in enumerate(rebalancing_investments):
+        rebalancing_investments[counter] = round(
+            rebalancing_investments[counter] +
+            post_rebalancing_amount_per_position,
+            post_comma_round)
+    return True, rebalancing_investments
+
+
+def sufficient_rebalancing_capital(investment_capital, rebalancing_investments):
+    """Checks whether investment capital is sufficient for rebalancing"""
+    total_investment_needed = 0
+    for amount in rebalancing_investments:
+        total_investment_needed += amount
+    if total_investment_needed > investment_capital:
+        return False
+    return True
+
+
+def calculate_below_threshold(amounts, target_weights, investment):
+    """If the investment is not sufficient we need to calculate the minimum"""
+    current_values = np.array(amounts)
+    current_total = current_values.sum()
+    current_weights = current_values / current_total
+
+    # calculate deviation of current weights from target weights
+    deviation = current_weights - target_weights
+
+    # find the index of the asset with the highest positive deviation
+    index_highest_positive_deviation = np.where(deviation == deviation[deviation > 0].max())[0]
+    target_portfolio_total = amounts[
+        index_highest_positive_deviation[0]] / target_weights[index_highest_positive_deviation[0]]
+    target_portfolio_values = np.array(target_portfolio_total * target_weights)
+    target_values = np.copy(current_values)
+    while investment > 0:
+        deviation = target_values - target_portfolio_values
+        index_highest_deviation = np.argmax(np.abs(deviation))
+        target_values[index_highest_deviation] += 1
+        investment -= 1
+    rebalancing_values = target_values - current_values
+    return rebalancing_values, target_values
+
+
+def user_interface(names, distribution_targets):
+    """Command line interface"""
+    amounts = []
+    print(f"\nPlease insert your current portfolio position values in {CURRENCY}:\n")
+    for count, name in enumerate(names):
+        while True:
+            try:
+                amount = int(input(f"{name}: \t"))
+                if amount >= 0:
+                    amounts.append(amount)
+                else:
+                    raise ValueError()
+                break
+            except ValueError:
+                print("Please write a valid positive number.")
+    table = PrettyTable()
+    table.field_names = ["Position", "Value", "Distribution"]
+    for count, name in enumerate(names):
+        table.add_row([name, f"{amounts[count]:.2f} {CURRENCY}",
+                      f"{amounts[count]*100/sum(amounts):.2f} %"])
+    table.add_row(["", "", ""])
+    table.add_row(["Total portfolio value", f"{sum(amounts):.2f} {CURRENCY}", ""])
+    print("\n Your current portfolio:")
+    print(table)
+
+    minimum_amounts = calculate_minimum_rebalancing_amounts(
+        amounts, distribution_targets)
+    minimum_amount = sum(minimum_amounts)
+    print(
+        f"\nTo rebalance fully, you need to invest a minimum of {minimum_amount:.2f} {CURRENCY}\n\n")
+    if input("- Show portfolio fully rebalanced? (y/n)\t") == "y":
+        rebalance_table = PrettyTable()
+        rebalance_table.field_names = ["Position", "Value", "Distribution"]
+        for count, name in enumerate(names):
+            total = amounts[count] + minimum_amounts[count]
+            rebalance_table.add_row([name, f"{total:.2f} {CURRENCY}",
+                                     f"{distribution_targets[count]*100:.2f} %"])
+        rebalance_table.add_row(["", "", ""])
+        rebalance_table.add_row(["Total",
+                                 f"{(sum(amounts) + sum(minimum_amounts)):.2f} {CURRENCY}", ""])
+        print(
+            f"\n Your portfolio after rebalancing with {minimum_amount:.2f} {CURRENCY}:")
+        print(rebalance_table)
+    while True:
+        try:
+            available_capital = int(
+                input("\nHow much do you want to invest?\n\n- Amount: "))
+            break
+        except ValueError:
+            print("\nInput invalid. Please write a valid positive number.")
+    rebalancing_possible, rebalancing_amounts = calculate_exact_rebalancing_amounts(
+        available_capital, 3, amounts, distribution_targets)
+    actual_distributions = distribution_targets
+    if not rebalancing_possible:
+        numpy_reb_amounts, target_amounts = calculate_below_threshold(
+            amounts, distribution_targets, available_capital)
+        rebalancing_amounts = numpy_reb_amounts.tolist()
+        investments = target_amounts.tolist()
+        actual_distributions = calculate_current_distributions(investments)
+    rebalance_amounts_table = PrettyTable()
+    rebalance_amounts_table.field_names = [
+        "Position", "Rebalancing amount"]
+    for count, name in enumerate(names):
+        rebalance_amounts_table.add_row(
+            [name, f"{rebalancing_amounts[count]:.2f} {CURRENCY}"])
+    print("\n Your rebalancing amounts are:")
+    print(rebalance_amounts_table)
+    # Create rebalance total table
+    rebalance_total_table = PrettyTable()
+    rebalance_total_table.field_names = [
+        "Position", "Value", "Distribution"]
+    for count, name in enumerate(names):
+        rebalance_total_table.add_row([name,
+                                        f"{(amounts[count] + rebalancing_amounts[count]):.2f} {CURRENCY}",
+                                        f"{actual_distributions[count]*100:.2f} %"])
+    rebalance_total_table.add_row(["", "", ""])
+    rebalance_total_table.add_row(["Total",
+                                    f"{sum(amounts) + sum(rebalancing_amounts):.2f} {CURRENCY}", ""])
+    print("\n Your portfolio after rebalancing:")
+    print(rebalance_total_table)
+    print("\n")
+
+# ONLY EDIT BELOW THIS LINE
+
+CURRENCY = "€"
+investment_portfolio = [
+    "iShares MSCI World Small Cap",
+    "iShares Core MSCI EM IMI",
+    "xTrackers MSCI World Value",
+    "xTrackers MSCI World Quality",
+    "xTrackers MSCI World Momentum"]
+target_distributions = np.array([
+    0.20, # 20% iShares MSCI World Small Cap
+    0.20, # 20% iShares Core MSCI EM IMI
+    0.20, # 20% xTrackers MSCI World Value
+    0.20, # 20% xTrackers MSCI World Quality
+    0.20 # 20% xTrackers MSCI World Momentum
+    ])
+    
+# ONLY EDIT ABOVE THIS LINE
+
+if not correct_input(investment_portfolio, target_distributions):
+    print("Input wrong.")
+else:
+    user_interface(investment_portfolio, target_distributions)
